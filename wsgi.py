f064dc2290c8c88a914ac9009e541e152b0cb819
bishalgkchwjhcb <bishalgogoi09735@gmaul.com>
2025-01-10 16:08:20 +0530

    Clean up project structure and update deployment configuration

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,8 @@
 from app import create_app
 from app.database import init_db
-import os
 
 app = create_app()
 init_db(app)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
+    app.run()
